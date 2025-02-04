1792e3b5ad97664b43c7ddf88c50b25289469776
Sri Gayathri <106133400+srigayathri777@users.noreply.github.com>
2025-02-04 11:16:34 +0530

    Time: 4 ms (81.09%), Space: 17.8 MB (33.22%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+sol = Solution()
+print(sol.isPalindrome(121))  
+print(sol.isPalindrome(-121))
+print(sol.isPalindrome(10))   
+
+
+        
